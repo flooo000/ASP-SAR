9c0e1ffb2a6f8cc7343dc40edc7b00d0af3a2361
flooo000 <florianleder@gmx.de>
2024-02-22 16:30:35 +0100

    add pixel information option

--- utils/check_correlation.py
+++ utils/check_correlation.py
@@ -6,7 +6,7 @@ check_correlation.py
 Plots the histogramm and the standard deviation of a selected area of an image.
 By default it reads .tif files, but other file formats are supported (.r4/.bil) as long is .hdr file exists
 
-Usage: check_correlation.py --data=<path> --area=<values> [--nsbas]
+Usage: check_correlation.py --data=<path> --area=<values> [--nsbas] [--xp=<values>] [--yp=<values>]
 check_correlation.py -h | --help
 
 Options:
@@ -14,7 +14,8 @@ Options:
 --data              Path to input image
 --area              Select a specific region xmin,xmax,ymin,ymax
 --nsbas             Input file format .r4/.bil (result of NSBAS processing) [Default: .tif]
-
+--xp                Pixel coordinates x
+--yp                Pixel coordinates y
 """
 
 ##########
@@ -67,11 +68,11 @@ def crop_data(data, xmin, xmax, ymin, ymax):
 
 def get_crop_statistics(crop):
 
-    print('Median of crop: {}'.format(np.nanmedian(crop)))
-    print('Mean of crop: {}'.format(np.nanmean(crop)))
-    print('Standard deviation of crop: {}'.format(np.nanstd(crop)))
+    print('Median of crop: {:.4f}'.format(np.nanmedian(crop)))
+    print('Mean of crop: {:.4f}'.format(np.nanmean(crop)))
+    print('Standard deviation of crop: {:.4f}'.format(np.nanstd(crop)))
 
-def plot_crop(input_file, area_vals, nsbas):
+def plot_crop(input_file, area_vals, nsbas, xpix, ypix):
 
     if(nsbas):
         data, ncol, nrow = read_from_nsbas_file(input_file)
@@ -83,11 +84,38 @@ def plot_crop(input_file, area_vals, nsbas):
     crop = crop_data(data, xmin, xmax, ymin, ymax)
     get_crop_statistics(crop)
 
+
+    if(xpix and ypix):
+        xlist = xpix.split(',')
+        ylist = ypix.split(',')
+
+        if(len(xlist) != len(ylist)):
+            print('Dimensions of xpix and ypix are not the same')
+
+        npix = len(xlist)
+
+        for i in range(npix):
+            x, y = int(xlist[i]), int(ylist[i])
+            value = data[y][x]
+            print('Value of coordinate ({},{}): {:.4f}'.format(x, y, value))
+
     fig = plt.figure(constrained_layout=True)
     axs = fig.subplot_mosaic([['Left', 'TopRight'],['Left', 'BottomRight']],
                           gridspec_kw={'width_ratios':[2, 1]})
+   
+   
+    # can also be done as input parameter if needed
+    vmax = np.nanpercentile(data, 98)
+    vmin = np.nanpercentile(data, 2)
 
-    im = axs['Left'].imshow(data)
+    im = axs['Left'].imshow(data, vmax=vmax,vmin=vmin)
+    
+    if(xpix and ypix):
+        for i in range(npix):
+            x, y = int(xlist[i]), int(ylist[i])
+            axs['Left'].scatter(x, y, color='r', marker='x', s=10)
+            axs['Left'].text(x, y, i, color='k', fontsize=10)
+
     axs['Left'].add_patch(Rectangle((xmin,ymin), xmax-xmin, ymax-ymin,edgecolor='r', fill=False))
     fig.colorbar(im)
 
@@ -95,6 +123,12 @@ def plot_crop(input_file, area_vals, nsbas):
 
     axs['TopRight'].imshow(crop)
     axs['TopRight'].set_title('Cropped Image')
+    
+    # Find a way to plot point in cropped area too - problem with changed image dimension 
+    #if(xpix and ypix):
+    #    for i in range(npix):
+    #        x, y = int(xlist[i]), int(ylist[i])
+    #        axs['TopRight'].scatter(x, y, color='r', marker='x')
 
     axs['BottomRight'].hist(crop, bins=20)
     axs['BottomRight'].set_title('Histogram')
@@ -115,6 +149,11 @@ area = arguments['--area']
 # check if it is file from nsbas processing (.r4)
 nsbas = arguments['--nsbas']
 
+# get coordinates of pixel
+xpix = arguments['--xp']
+ypix = arguments['--yp']
+
+
 # maybe add later option to not plot only an area
 if(area):
     # xmin, xmax - columns
@@ -123,5 +162,5 @@ if(area):
 else:
     print('none')
 
-plot_crop(input_file, area_vals, nsbas)
+plot_crop(input_file, area_vals, nsbas, xpix, ypix)
 
