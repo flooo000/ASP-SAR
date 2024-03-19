705160d3f0fe236d56f9bcd0ab510ca3a8e2da65
flooo000 <florianleder@gmx.de>
2024-03-19 11:18:27 +0100

    change interpolation method

--- utils/generate_lin_vector_file.py
+++ utils/generate_lin_vector_file.py
@@ -37,12 +37,11 @@ def lin_trend(dates, d1, d2):
     i1 = dates.index(d1)
     i2 = dates.index(d2)
 
-    m = 1 / (i2 - i1)
     v = np.zeros((len(dates)))
-    for i in range(i1, i2 + 1):
-        v[i] = m * (i - i1)
+    for i in range(i1 + 1, i2):
+        v[i] = np.nan
 
-    for i in range(i2 + 1, len(dates)):
+    for i in range(i2, len(dates)):
         v[i] = 1.0
     
     return v
@@ -69,8 +68,15 @@ name = arguments['--name']
 
 list_dates = pd.read_csv(data_path, sep=' ', header=None)
 dates = [str(d) for d in list(list_dates[0])]
+dates_dec = [float(d) for d in list(list_dates[1])]
 
-vector = lin_trend(dates, start_date, end_date)
+# set values to NaN that will be interpolated, date1=0 and date2=1
+vector_nan = lin_trend(dates, start_date, end_date)
+
+# generate pd.Series to interpolate based on decimal date and replace nan values
+vector_series = pd.Series(vector_nan, index=dates_dec)
+
+vector = vector_series.interpolate(method='index')
 
 with open(os.path.join(dest_path, 'vector_lin{}.txt'.format(name)), 'w') as f:
     for v in vector:
