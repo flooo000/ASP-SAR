cbf09779f8b264f7936aa2cfc852bdb82d37dd9a
flooo000 <florianleder@gmx.de>
2024-11-25 11:40:59 +0100

    improved

--- projection/merge_extra_files.py
+++ projection/merge_extra_files.py
@@ -25,27 +25,42 @@ import numpy as np
 import pandas as pd
 from pathlib import Path
 import docopt
-import datetime
+from datetime import datetime
 
 #############
 # FUCNTIONS #
 #############
 
+def date_to_float(date):
+    date_str = str(date)
+    d = datetime.strptime(date_str, '%Y%m%d')
+    
+    return (d.year + (d.month-1)/12.0 + (d.day-1)/365.0)
+
 def merge_img_ret_files(imgret1_file, imgret2_file):
 
-    header = ['index', 'date', 'nimg', 'date_dec', 'Bt', 'Bperp']
+    header1 = ['index', 'date', 'nimg', 'date_dec', 'Bt', 'Bperp1']
+    header2 = ['index', 'date', 'nimg', 'date_dec', 'Bt', 'Bperp2']    
     
     imgret1 = pd.read_csv(imgret1_file, header=None, sep='\s+')
+    imgret1.columns = header1
     imgret2 = pd.read_csv(imgret2_file, header=None, sep='\s+')
+    imgret2.columns = header2
 
+    # sort by date_dec
     combined_imgret = pd.concat([imgret1, imgret2], ignore_index=True)
-    combined_imgret.columns = header
     combined_imgret = combined_imgret.sort_values(by=['date_dec'])
-
     new_index = range(1, len(combined_imgret) + 1)
 
-    combined_imgret['index'] = new_index
+    # set value to 0 where data is not available
+    combined_imgret['Bperp1'] = combined_imgret['Bperp1'].fillna(0)
+    combined_imgret['Bperp2'] = combined_imgret['Bperp2'].fillna(0)
 
+    combined_imgret['index'] = new_index
+    # update Bt column to have everything relative to merged first date
+    combined_imgret['Bt'] = (combined_imgret['date'].apply(date_to_float) - date_to_float(combined_imgret['date'][0])).apply(lambda x: f"{x:.6f}")
+    
+    print(combined_imgret)
     return combined_imgret
 
 
@@ -88,7 +103,7 @@ rmsdate2_file = os.path.join(os.path.dirname(imgret2_file), 'RMSdate')
 dest_path = arguments['--dest']
 
 combined_imgret = merge_img_ret_files(imgret1_file, imgret2_file)
-
+print(combined_imgret)
 combined_rmsdate = merge_rms_date_files(rmsdate1_file, rmsdate2_file)
 
 
